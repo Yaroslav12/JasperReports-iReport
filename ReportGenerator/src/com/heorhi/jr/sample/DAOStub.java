51e764a4164a94e9da2b05db5d0627483bb53166
Heorhi-Puhachou <h.puhachou@gmail.com>
2014-12-12 11:16:48 +0300

    Working version

--- ReportGenerator/src/com/heorhi/jr/sample/DAOStub.java
+++ ReportGenerator/src/com/heorhi/jr/sample/DAOStub.java
@@ -3,7 +3,7 @@ package com.heorhi.jr.sample;
 import java.util.ArrayList;
 
 public class DAOStub {
-	
+
 	public ArrayList<DataBean> getDataBeanList() {
 		ArrayList<DataBean> dataBeanList = new ArrayList<DataBean>();
 		DataBean tempBean = new DataBean();
@@ -11,13 +11,13 @@ public class DAOStub {
 		tempBean.setDiscipline("Физика");
 		tempBean.setAbsence(2);
 		dataBeanList.add(tempBean);
-		
+
 		tempBean = new DataBean();
 		tempBean.setName("Вася");
 		tempBean.setDiscipline("История");
 		tempBean.setAbsence(3);
 		dataBeanList.add(tempBean);
-		
+
 		tempBean = new DataBean();
 		tempBean.setName("Ира");
 		tempBean.setDiscipline("English");
--- ReportGenerator/src/com/heorhi/jr/sample/Generator.java
+++ ReportGenerator/src/com/heorhi/jr/sample/Generator.java
@@ -2,9 +2,9 @@ package com.heorhi.jr.sample;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
@@ -18,29 +18,24 @@ import net.sf.jasperreports.engine.xml.JRXmlLoader;
 public class Generator {
 
 	String pathForSaving = "D:\\Free time\\TestResult.pdf";
-	String pathForPattern = "D:\\Free time\\workspace\\JasperReports-iReport\\ReportGenerator\\test_jasper.jrxml";
-	//String pathForSaving = "/home/heorhi/TestResult.pdf"
-	//String pathForPattern = "/home/heorhi/test_jasper.jrxml";
+	String pathForPattern = "D:\\Free time\\workspace\\JasperReports-iReport\\ReportGenerator\\jrxml\\first_template.jrxml";
+
+	// String pathForSaving = "/home/heorhi/TestResult.pdf"
+	// String pathForPattern = "/home/heorhi/test_jasper.jrxml";
 	public void create() throws JRException {
 		DAOStub dataBeanMaker = new DAOStub();
 		ArrayList<DataBean> dataBeanList = dataBeanMaker.getDataBeanList();
-
 		JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(
 				dataBeanList);
-
 		Map<String, Object> parameters = new HashMap<String, Object>();
-
+		parameters.put("DATE", new Date());
 		File reportPattern = new File(pathForPattern);
-
 		JasperDesign jasperDesign = JRXmlLoader.load(reportPattern);
 		JasperReport jasperReport = JasperCompileManager
 				.compileReport(jasperDesign);
 		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,
 				parameters, beanColDataSource);
-		JasperExportManager.exportReportToPdfFile(jasperPrint,
-				pathForSaving);
-		
-
+		JasperExportManager.exportReportToPdfFile(jasperPrint, pathForSaving);
 	}
 
 	public static void main(String[] args) {
@@ -49,7 +44,7 @@ public class Generator {
 			new Generator().create();
 			System.out.println("Генерация отчёта завершена");
 		} catch (Exception e) {
-			System.out.println("Во время генерации возникла ошибка: "+e);
+			System.out.println("Во время генерации возникла ошибка: " + e);
 		}
 	}
 }
